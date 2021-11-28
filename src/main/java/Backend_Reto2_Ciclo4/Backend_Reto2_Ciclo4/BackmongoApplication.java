f58d859871a75962d2e7795339fd604f172c1ab8
fabianhernandezmt <91509505+fabianhernandezmt@users.noreply.github.com>
2021-11-28 00:50:52 -0500

    Add files via upload

---
+++ src/main/java/Backend_Reto2_Ciclo4/Backend_Reto2_Ciclo4/BackmongoApplication.java
@@ -0,0 +1,13 @@
+package Backend_Reto2_Ciclo4.Backend_Reto2_Ciclo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Backend_Reto2_Ciclo4/Backend_Reto2_Ciclo4/BackmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend_Reto2_Ciclo4.Backend_Reto2_Ciclo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
